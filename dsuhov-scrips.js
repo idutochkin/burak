4f575b5d07f5192608b59fa771482377186e59be
Dmitriy Sukhov <dsujev@gmail.com>
2020-11-27 01:04:52 +0300

    downl btn

--- dsuhov-scrips.js
+++ dsuhov-scrips.js
@@ -1,3 +1,11 @@
 window.document.addEventListener("DOMContentLoaded", function () {
-  console.log(";sdf");
+  const photoEL = document.querySelector(".ds_main__img");
+  const downlEl = document.querySelector(".ds_download-btn");
+
+  const imgUrl = localStorage.getItem("photoUrl");
+
+  if (imgUrl) {
+    photoEL.src = imgUrl;
+    downlEl.setAttribute("href", imgUrl);
+  }
 });
